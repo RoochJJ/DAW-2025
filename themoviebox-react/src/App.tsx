0d154d3378d0e3aee10d10aaac3aa015db4d2ec5
RoochJJ <jackethanvertus@gmail.com>
2025-10-30 21:58:02 -0300

    Aula-30-10-2025

--- themoviebox-react/src/App.tsx
+++ themoviebox-react/src/App.tsx
@@ -1,11 +1,14 @@
 import { Footer } from "./components/layout/footer";
 import { Header } from "./components/layout/header";
+import { Navbar } from "./components/layout/navbar";
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Navbar/>
+      </main>
       <Footer />
     </>
   );
