61dce58325fb6b9c093d7e5a3b1b2baf8b43963a
RoochJJ <jackethanvertus@gmail.com>
2025-11-06 19:19:33 -0300

    Algumas correções, antes da aula dia 06/11

--- themoviebox-react/src/assets/cases/movies/components/movie-content.tsx
+++ themoviebox-react/src/assets/cases/movies/components/movie-content.tsx
@@ -2,22 +2,17 @@ import { useEffect, useState } from "react";
 import { MovieService, type MovieDTO } from "../services/movie.service";
 
 export function MovieContent() {
-    const [, setMovies] = useState<MovieDTO[]> ([]);
+  const [movies, setMovies] = useState<MovieDTO[]>([]);
 
-    useEffect(() => {
-        MovieService.list().then((result) => {
-            setMovies(result);
-        })
-    }, []);
+  useEffect(() => {
+    MovieService.list().then((result) => {
+      setMovies(result);
+    });
+  }, []);
 
-
-    return (
-        <section
-            className="bg-[#1c1c1c] padding-8 grid grid-cols-[repeat(auto-fill,minmax(200x,1fr))] gap-8 max-w-6xl max-auto"
-        >
-             <p>(JSON.stringify(movies))</p>
-
-        </section>
-    )
-    
+  return (
+    <section className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill, minmax(200px, 1fr))] gap-8 max-w-6xl mx-auto">
+      <p>{JSON.stringify(movies)}</p>
+    </section>
+  );
 }
