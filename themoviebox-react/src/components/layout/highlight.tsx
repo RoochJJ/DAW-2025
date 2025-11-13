10942ce54b042f3011e771e274a6f6f5ca77fa66
RoochJJ <jackethanvertus@gmail.com>
2025-11-13 17:02:39 -0300

    imagen com mais fosco

--- themoviebox-react/src/components/layout/highlight.tsx
+++ themoviebox-react/src/components/layout/highlight.tsx
@@ -1,13 +1,10 @@
 export function Highlight() {
   return (
-    <section className="relative flex bg-[url('../img/etT14XfDEqhQZdD47ywpyihXPyW.jpg')] bg-left bg-cover bg-no-repeat">
-      <div className="absolute top-0 left-0 w-full h-full z-1 bg-[linear-gradient(to_right,rgba(17,17,17,1)_calc((50vw-70px)-340px),
-      rgba(17,17,17,0.7)_50%,rgba(17,17,17,0.7)_100%)]">
-
-      </div>
-
-      <div className="flex gap-8 py-8 z-2 mx-auto max-w-[1140px]">
-
+    <section className="relative flex bg-[url('../img/etT14XfDEqhQZdD47ywpyihXPyW.jpg')] bg-left
+                                                   bg-top bg-cover bg-no-repeat">
+      <div className="absolute top-0 left-0 w-full h-full z-1 bg-[linear-gradient(to_right,rgba(17,17,17,1)_calc((50vw-70px)-340px),rgba(17,17,17,0.7)_50%,rgba(17,17,17,0.7)_100%)]"></div>
+      <div className="flex gap-8 py-8 z-2 mx-auto max-w-[1140px]"> 
+        
       </div>
       <div
         className="absolute top-0 left-0 w-full h-full z-10"
