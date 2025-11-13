3b70b9aeddc142d151fe9562d815650eba462812
RoochJJ <jackethanvertus@gmail.com>
2025-11-12 23:18:57 -0300

    Criação do Highlight e ajuste do Themoviebox no canto inferior da tela

--- themoviebox-react/src/components/layout/header.tsx
+++ themoviebox-react/src/components/layout/header.tsx
@@ -3,7 +3,7 @@ export function Header() {
         <header className="py-4 px-8">
             <div className="max-w-6xl mx-auto flex flex-row items-center justify-between">
                 <h1 className="text-2xl font-bold tracking-wide text-wide">
-                    <span className="font-normal">THE</span>MOVIEBOX</h1>
+                    <span className="font-normal">THE</span> MOVIEBOX</h1>
                 <div className="flex gap-4">
                     <a className="text-white text-sm py-2 px-4 border-[#333] rounded transition hover:bg-[#333]" href="#">
                         Entrar
